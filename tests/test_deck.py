5e671bced15f2b59836c6f051fdb6bb2852ce7e6
Victor Sanchez <thespielmaster@MacBook-Pro-de-Victor.local>
2025-05-11 21:30:34 +0200

    more deck tests

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -24,10 +24,20 @@ class TestModifiersFunctions(unittest.TestCase):
     def test_remaining_cards(self):
         self.assertEqual(self.deck.remaining_cards(), 52)
 
-    
     def test_remaining_cards_after_draw(self):
         self.assertEqual(type(self.deck.draw()), Card)
         self.assertEqual(self.deck.remaining_cards(), 51)
+
+    def test_draw_until_deck_empty(self):
+        
+        cards_count = self.deck.remaining_cards()
+        
+        while not self.deck.is_empty():
+            self.assertEqual(type(self.deck.draw()), Card)
+            cards_count -= 1
+            self.assertEqual(self.deck.remaining_cards(), cards_count)
+
+        self.assertEqual(self.deck.is_empty(), True)
 
 class TestBooleanFunctions(unittest.TestCase):
 
